97f1d560effa5ce919175cdd9a1696bb6d88e4ef
mr-isik <faruk_isik_24@protonmail.com>
2025-08-07 15:31:05 +0300

    refactor(products): remove unused imports in product detail page

--- src/app/(client)/[productSlug]/page.tsx
+++ src/app/(client)/[productSlug]/page.tsx
@@ -1,9 +1,6 @@
 // import { useParams } from 'next/navigation';
 import { ProductDetail } from '@/entities/product/ui/product-detail';
-import { mockProductDetail } from '@/entities/product/api/mock-data';
-import type { ProductVariant } from '@/entities/product/model';
 import MaxWidthWrapper from '@/shared/ui/max-width-wrapper';
-import { useQueryProductDetail } from '@/entities/product/queries';
 
 export default async function ProductDetailPage({
     params,
